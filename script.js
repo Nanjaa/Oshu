d0b4411279182cdc700cf167c033ea60bfead2e6
Nanjaa <steph.o-91@hotmail.com>
2015-05-20 00:58:47 -0500

    created a life bar that would deplete over time and stop at 0

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ $(document).ready(function() {
 
 	var cookies = 100;
 
-	// $('#test').click(function() {
-	// 	console.log("test");
-	// });
-
 	$('#test').click(function() {
 		cookieClick(1);
 	});
@@ -15,7 +11,60 @@ $(document).ready(function() {
 	function cookieClick(number) {
 		cookies = cookies - number;
 		$('#number').text(cookies);
+		var percent = cookies + "%";
+		$('#life').css('width', percent);
+		if(cookies >= 66) {
+			$('#life').css('background-color', 'green');
+		}
+		else if(cookies >= 33) {
+			$('#life').css('background-color', 'orange');
+		}
+		else {
+			$('#life').css('background-color', 'red');
+		}
 	};
+
+	$('#test2').click(function() {
+		buyCursor();
+	});
+
+	var cursors = 1;
+
+	function buyCursor() {
+		var cursorCost = Math.floor(10 * Math.pow(1.1, cursors));
+		if(cookies >= cursorCost) {
+			cursors = cursors + 1;
+			cookies = cookies - cursorCost;
+			$('#number').text(cookies);
+			$('#cursors').text(cursors);
+		};
+		var nextCost = Math.floor(10 * Math.pow(1.1, cursors));
+		$('#cursorCost').text(nextCost);
+	};
+
+	var life = setInterval(function() {
+		if(cookies === 0) {
+			console.log('finished');
+			clearInterval(life);
+			return;
+		}
+		cookieClick(cursors);
+	}, 100);
+
+	window.setInterval(life);
+
+	$('#stop').click(function() {
+		clearInterval(life);
+	})
+
+	// window.setInterval(function() {
+	// 	if(cookies === 0) {
+	// 		console.log('finished');
+	// 		clearInterval();
+	// 		return;
+	// 	}
+	// 	cookieClick(cursors);
+	// }, 10);
 
 
 
