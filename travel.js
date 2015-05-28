37525b5c37c47ace57b710c5047c306c1d7e5dd4
Nanjaa <steph.o-91@hotmail.com>
2015-05-28 01:11:10 -0500

    almost finished with my lose time universal function, began the music for Tyrianne

--- travel.js
+++ travel.js
@@ -4,61 +4,63 @@ $(document).ready(function() {
 
 	$('.planet').mouseover(function() {
 		var distanceCost = Math.abs(($(this).attr('distance') - myLocation.current) * 1.5);
-		console.log(distanceCost);
+		var distanceBar = distanceCost * 60;
+
 		var currentTime = $('#minutes').text();
 		var newTime = currentTime - distanceCost;
+		console.log(distanceCost);
+
+
 		$('.planet').click(function() {
 			if(newTime <= 0) {
-				console.log(distanceCost);
-				console.log(newTime);
 				console.log("not possible!");
 				error.play();
 			}
 			else {
-				$('#minutes').text(newTime);
 				myLocation.current = $(this).attr('distance');	
-				highlightPlanet();
-				$(this).css('color', 'white');
-				$(this).attr('selected', 'yes');
+				// highlightPlanet();
+				lifeEvent(distanceCost, distanceBar);
+				// $(this).css('color', 'white');
+				// $(this).attr('selected', 'yes');
 			}
 		});
 	});
 
-	var colored = false;
-	var attr = $('#item').attr('selected');
-	if(attr === 'yes') {
-		if (colored) {
-			$('#item').css('color', 'white');
-			colored = false;
-		}
-		else {
-			$('#item').css('color', 'originalcolor');
-			colored = true;
-		}
-	}
+	// var colored = false;
+	// var attr = $('#item').attr('selected');
+	// if(attr === 'yes') {
+	// 	if (colored) {
+	// 		$('#item').css('color', 'white');
+	// 		colored = false;
+	// 	}
+	// 	else {
+	// 		$('#item').css('color', 'originalcolor');
+	// 		colored = true;
+	// 	}
+	// }
 
-	function highlightPlanet() {
-		$('#Luneda').css('color', '#c1e3da');
-		$('#Kanedos').css('color', '#ff9a00');
-		$('#Tyrianne').css('color', '#db1600');
-		$('#Kaprika').css('color', '#1c9600');
-		$('#AliNada').css('color', '#969696');
-		$('#Carpic').css('color', '#5650ff');
+	// function highlightPlanet() {
+	// 	$('#Luneda').css('color', '#c1e3da');
+	// 	$('#Kanedos').css('color', '#ff9a00');
+	// 	$('#Tyrianne').css('color', '#db1600');
+	// 	$('#Kaprika').css('color', '#1c9600');
+	// 	$('#AliNada').css('color', '#969696');
+	// 	$('#Carpic').css('color', '#5650ff');
 
-		$('#Luneda').attr('selected', 'no');
-		$('#Kanedos').css('color', '#ff9a00');
-		$('#Tyrianne').css('color', '#db1600');
-		$('#Kaprika').css('color', '#1c9600');
-		$('#AliNada').css('color', '#969696');
-		$('#Carpic').css('color', '#5650ff');
-	};
+	// 	$('#Luneda').attr('selected', 'no');
+	// 	$('#Kanedos').css('color', '#ff9a00');
+	// 	$('#Tyrianne').css('color', '#db1600');
+	// 	$('#Kaprika').css('color', '#1c9600');
+	// 	$('#AliNada').css('color', '#969696');
+	// 	$('#Carpic').css('color', '#5650ff');
+	// };
 
 
-	function hightlightplanettest() {
-		for (var i = 0; i < planets.length; i++) {
-			$(planets[i].id).css('color', planets[i].color);
-		}
-	};
+	// function hightlightplanettest() {
+	// 	for (var i = 0; i < planets.length; i++) {
+	// 		$(planets[i].id).css('color', planets[i].color);
+	// 	}
+	// };
 
 
 
--- universal.js
+++ universal.js
@@ -1,16 +1,17 @@
-function lifeEvent(loss) {
-	remainingLife = remainingLife - 1;
+function lifeEvent(timer, bar) {
+// calculate loss
+console.log('timer' + timer);
+console.log('bar ' + bar);
+	var remainingLife = $('#remainingLife').attr('remainingLife');
+	remainingLife = remainingLife - bar;
 	$('#remainingLife').attr('remainingLife', remainingLife);
+
+	var currentTime = $('#minutes').text();
+	var newTime = currentTime - timer;
+// display loss
 	var percent = ((remainingLife/3600)*100) + "%";
 	$('#life').css('width', percent);
-	if(remainingLife >= 2400) {
-		$('#life').css('background-color', 'green');
-	}
-	else if(remainingLife >= 1200) {
-		$('#life').css('background-color', 'orange');
-	}
-	else {
-		$('#life').css('background-color', 'red');
-	}
 	$('#minutes').text(newTime);
+
+	return;
 }
