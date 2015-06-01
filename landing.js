a764dd9c264ea2e1ea0e515b3c2abeea6b56f8d6
Nanjaa <steph.o-91@hotmail.com>
2015-05-31 23:53:22 -0500

    added a landing page and began work on that, including new audio files

---
+++ landing.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+	// var text = {
+	// 	introduction: 'Hello, my name is Oh Shoo. I am an android. This is the year twenty one seventy five. Androids, humans, aliens, and other species live together in harmony. 
+	// 	As an android, I have a choice: repair my parts and live as long as possible, or make use of a program that will terminate me after a set amount of time. 
+	// 	I have chosen the latter. I wanted to be as human as possible, to feel like I really belonged to my family. My time has almost come to an end, and I can see 
+	// 	my internal clock depleting quickly. However, there are still many things I wish to see and do before I pass. Ive made a list of the experiences I crave most, 
+	// 	and now set out to complete that list. I dont have much time, but Im excited that my last hours will be beautiful, and full of rich and wonderful memories.'
+	// };
+
+
+
+
+
+
+
+
+
+
+	$('#clickPlay').click(function() {
+		console.log('hello');
+		$('#landing').hide();
+		$('#welcome').show();
+	});
+
+});
+
