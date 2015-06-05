5b4432d3ab8b8bf412aa843eefc13cebcc22d07f
Nanjaa <steph.o-91@hotmail.com>
2015-06-05 04:42:58 -0500

    created the map for the beach on Luneda

--- luneda.js
+++ luneda.js
@@ -17,8 +17,29 @@ $(document).ready(function() {
 	});
 	$('.visitSea').click(function() {
 		$('#beach').show();
+		$('.lunedaInteract').writeText(beachText.welcome);
 		console.log('beach');
 	});
+
+	(function($) {
+		$.fn.writeText = function(content) {
+			$('.lunedaInteract').text('');
+			var contentArray = content.split(""),
+				current = 0,
+				elem = this;
+				setInterval(function() {
+					if(current < contentArray.length) {
+						elem.text(elem.text() + contentArray[current++]);
+					}
+				}, 30);
+		};
+	}) (jQuery);
+
+	var beachText = {
+		welcome: "You have arrived on the beaches of the Electric Sea. There is a buzzing sound that mixes gently with the sound of the waves crashing."
+	};
+
+
 
 
 
