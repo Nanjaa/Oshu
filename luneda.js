e9a960b2fc91cfa7f3399c2653d93685f3e54507
Nanjaa <steph.o-91@hotmail.com>
2015-06-09 15:28:44 -0500

    finished the interaction with the rental on the beach

--- luneda.js
+++ luneda.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 
+	var beachText = {
+		inUse: false,
+		welcome: "You have arrived on the beaches of the Electric Sea. There is a buzzing sound that mixes gently with the sound of the waves crashing.",
+		rentalWelcome: "Hello there young lady! I have everything you'll need to swim in the seas without getting yourself killed. What do you say? 50 coins covers it all!",
+		rentalNoCoins: "That's not enough money! 50 coins or no deal. I don't bargain.",
+		rentalComplete: "Fantastic! Here's your ElectroSuit. And because you seem like a nice gal, here's a camera for you to take pictures underwater with. You have an hour to swim, then I'll need the equipment back. Have a great day!",
+		rentalOptions: "Do you want to rent an ElectroSuit?",
+		rentalInUse: "Go have fun with your equipment! But remember to bring it back...",
+		seaLife: "You step into the sea, and submerge yourself. There are fish everywhere. You try to take as many pictures as you can before you have to return the suit!",
+		seaDeath: "You step into the sea without protection. The electricity swallows you up, and you feel yourself losing control. Slowly, everything fades"	
+	};
+
 	function resetText() {
 		$('#lunedaInteract').writeText('');
 	};
@@ -32,20 +44,53 @@ $(document).ready(function() {
 		$('.lunedaInteract').writeText(beachText.welcome);
 	});
 
-
-	function testOne() {
-		console.log('hellotest');
-		$('.lunedaInteract').writeText(beachText.rentalWelcome)
-		return true;
-	};
-
-	function testTwo() {
-		console.log('finished yo');
-	};
-
 	$('#equipment').click(function() {
-		$.when(testOne).then(testTwo);
+		rentalActivate();
 	});
+
+	function rentalActivate() {
+		if(beachText.inUse == true) {
+			$('.lunedaInteract').writeText(beachText.rentalInUse);
+		}
+		else {
+			$('.lunedaInteract').writeText(beachText.rentalWelcome)
+			// checks when to ask for response
+			var rentalOptions = setInterval(function() {
+				if($('.lunedaInteract').text() == beachText.rentalWelcome) {
+					clearInterval(rentalOptions);
+					setTimeout($('.lunedaInteract').writeText(beachText.rentalOptions), 10000);
+				};
+			}, 1000);
+			// checks when to display yes/no
+			var displayRentalOptions = setInterval(function() {
+				if($('.lunedaInteract').text() == beachText.rentalOptions) {
+					clearInterval(displayRentalOptions);
+					function yesNo() {
+						$('.lunedaInteract').append('<ul><li class="options" id="yesRental">Yes</li><li class="options" id="noRental">No</li></ul>')
+					};
+					setTimeout(yesNo(), 1000);
+
+					$('.options').click(function() {
+						var coins = Oshu.coins;
+						if(coins >= 50) {
+							beachText.inUse = true;
+							coins = coins - 50;
+							Oshu.coins = coins;
+							$('#coinsAmt').text(coins);
+							$('.lunedaInteract').writeText(beachText.rentalComplete);
+						}
+						else {
+							$('.lunedaInteract').writeText(beachText.rentalNoCoins);
+						}
+					});
+				};
+			}, 1000);				
+		};
+	};
+
+	function clickYes() {
+
+	};
 
 	// $('#myShip').click(function() {
 	// 	console.log('wtf');
@@ -64,16 +109,6 @@ $(document).ready(function() {
 				}, 30);
 		};
 	}) (jQuery);
-
-	var beachText = {
-		welcome: "You have arrived on the beaches of the Electric Sea. There is a buzzing sound that mixes gently with the sound of the waves crashing.",
-		rentalWelcome: "Hello there young lady! I have everything you'll need to swim in the seas without getting yourself killed. What do you say? 50 coins covers it all!",
-		rentalNoCoins: "That's not enough money! 50 coins or no deal. I don't bargain.",
-		rentalComplete: "Fantastic! Here's your ElectroSuit. And because you seem like a nice gal, here's a camera for you to take pictures underwater with. You have an hour to swim, then I'll need the equipment back. Have a great day!",
-		rentalOptions: "Do you want to rent an ElectroSuit?",
-		seaLife: "You step into the sea, and submerge yourself. There are fish everywhere. You try to take as many pictures as you can before you have to return the suit!",
-		seaDeath: "You step into the sea without protection. The electricity swallows you up, and you feel yourself losing control. Slowly, everything fades"	
-	};
 
 
 
