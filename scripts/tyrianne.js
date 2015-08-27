bdd8fc502706a107c5ae8e3590bae90e1505d90f
Nanjaa <steph.o-91@hotmail.com>
2015-08-27 16:42:48 -0500

    All the locations are available now on Tyrianne

--- scripts/tyrianne.js
+++ scripts/tyrianne.js
@@ -1,7 +1,66 @@
 $(document).ready(function() {
 
-	// brings you to the planet from the system map
+	// brings up the common divs between all the city details
+	$('.tyrianneCity').click(function() {
+		$('.tyrianneCity').hide();
+		$('.return').show();
+		$('.planetInteraction').show();
+	})
 
+	// takes you back to the city map from any city details
+	function tyrianneReturn() {
+		$('.return').hide();
+		$('.cityDetails').hide();
+		$('.tyrianneCity').show();	
+	}
+	$('.return').click(function() {
+		tyrianneReturn();
+		changeLocation('#tyrianneMap');
+	});
+
+	// ________________________________________________________________
+	// | ==============================================================|
+	// |															   |
+	// |						LOCATIONS 	 						   |
+	// |															   |
+	// |===============================================================|
+	// |_______________________________________________________________|
+
+	var fuzzbuttEntry = false;
+
+	$('pre').click(function() {
+		var location = $(this).attr('id');
+		switch(location) {
+			case 'library': 
+				changeLocation('.library');
+				$('.library').show();
+				break;
+			case 'fuzzbuttFactory':
+				changeLocation('.fuzzbutt');
+				$('.fuzzbutt').show();
+				if(fuzzbuttEntry == false) {
+					$('.fuzzbuttDetails').hide();
+					$('#fuzzbuttDoorman').show();
+				}
+				else {
+					$('.fuzzbuttDetails').show();
+					$('#fuzzbuttDoorman').hide();
+				}
+				break;
+			case 'tyrianneMechanic':
+				changeLocation('.tyrianneMechanic');
+				$('.tyrianneMechanic').show();
+				break;
+			case 'poorMan':
+				changeLocation('.poorMan');
+				$('.poorMan').show();
+				break;
+			case 'tyrianneJewelry':
+				changeLocation('.tyrianneJewelry');
+				$('.tyrianneJewelry').show();
+				break;
+		};
+	});
 
 
 
