d90790c48befe197f50d5469612e1275b2df0f0e
Nanjaa <steph.o-91@hotmail.com>
2015-09-23 12:06:23 -0500

    Added hover functions to my buttons in the landing

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
 
-	// $('#play').mouseOver(function() {
-	// 	$('play').css('background-color', pink);
-	// })
-	// $('#play').mouseOUt(function() {
-	// 	$('play').css('background-color', black);
-	// })
+	// hover over the play button
+	$('#clickPlay').mouseenter(function() {
+		$('#play').css('background-color', 'green');
+	});
+	$('#clickPlay').mouseout(function() {
+		$('#play').css('background-color', 'black');
+	});
+	$('#play').mouseenter(function() {
+		$('#play').css('background-color', 'green');
+	});
+	$('#play').mouseout(function() {
+		$('#play').css('background-color', 'black');
+	});
 
+	// restart the music if it finishes
 	audio.addEventListener('ended', function() {
 		this.currentTime = 0;
 		this.play();
@@ -14,6 +22,7 @@ $(document).ready(function() {
 
 	play('music/introSong.wav');
 
+	// mute the game
 	$('.sounds').click(function() {
 		if($(this).attr('id') == 'soundsOn') {
 			$('#soundsOn').hide();
@@ -31,6 +40,7 @@ $(document).ready(function() {
 		}
 	});
 
+	// animate the twinkle of the stars and the flames of the ship
 	var twinkle = false;
 	var flames = false;
 
@@ -64,6 +74,7 @@ $(document).ready(function() {
 	twinkleAndFlames();
 
 
+	// click play
 	function playGame() {
 		$('#audio').animate({volume: .3}, 1000);
 		$('#artwork').animate({
@@ -93,7 +104,6 @@ $(document).ready(function() {
 		introduction: "Hello, my name is Oshu. I am an android. This is the year 2175. Androids, humans, aliens, and other species live together in harmony. As an android, I have a choice: repair my parts and live as long as possible, or make use of a program that will terminate me after a set amount of time. I have chosen the latter. I wanted to be as human as possible, to feel like I really belonged to my family. My time has almost come to an end, and I can see my internal clock depleting quickly. However, there are still many things I wish to see and do before I pass. I've made a list of the experiences I crave most, and now set out to complete that list. I don't have much time, but I'm excited that my last hours will be beautiful, and full of rich and wonderful memories."
 	};
 
-
 	function playText() {
 		oshuIntro.play();
 		$('#skip').show();
@@ -118,7 +128,18 @@ $(document).ready(function() {
 	function showInstructions() {
 		$('#welcome').hide();
 		$('#instructions').show();
-	}
+	};
+
+	// hover over the "click here to play" button
+
+	$('#begin').mouseenter(function() {
+		$('#begin').css('background-color', 'black');
+		$('#begin').css('color', 'white');
+	});
+	$('#begin').mouseout(function() {
+		$('#begin').css('background-color', 'white');
+		$('#begin').css('color', 'black');
+	});
 
 });
 
